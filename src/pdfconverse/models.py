c598f63097b295694373a40a02ff0926140f9748
Chencha Jacob <jmokua@gmail.com>
2024-06-29 15:24:02 +0100

    Better file error checking

--- src/pdfconverse/models.py
+++ src/pdfconverse/models.py
@@ -1,8 +1,14 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, validator
+from pathlib import Path
 
 class FilePath(BaseModel):
     path: str
+
+    @validator('path')
+    def validate_path(cls, v):
+        if not Path(v).is_file():
+            raise ValueError(f'The path {v} is not a valid file')
+        return v
 
 class GeminiSetup(BaseModel):
     api_key: str
